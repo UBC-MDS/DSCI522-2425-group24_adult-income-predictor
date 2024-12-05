5ef04c65a571cee8168f0787cb09838e011ef4b0
mikem2m <michael08ace@gmail.com>
2024-12-04 16:34:13 -0800

    Script 2

--- command.bash
+++ command.bash
@@ -1,3 +1,7 @@
 python scripts/download_data.py \
     --url="https://archive.ics.uci.edu/static/public/2/adult.zip" \
     --target_dir="data/raw"
+
+python scripts/read_and_validate.py \
+    --raw_dir="data/raw/adult.data" \
+    --processor_dir="data/processed"
