3196d3dd0dda745870fc35d6627bb8f6e7e6d40e
mikem2m <michael08ace@gmail.com>
2024-12-15 15:37:31 -0800

    cleanup lizzy's work

--- tests/test_generate_bar_chart_and_save.py
+++ tests/test_generate_bar_chart_and_save.py
@@ -4,10 +4,9 @@
 
 import sys
 import os
+import pandas as pd
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.generate_bar_chart_and_save import generate_bar_chart_and_save
-import pytest
-import pandas as pd
 
 # SETUP
 
@@ -15,17 +14,18 @@ test_dir= 'tests/folder_plot'
 plot_1 = 'plot_1.png'
 
 test_df = pd.DataFrame({
-    'age': ['10', '20', '27', '56', '33', '20', '27'],
-    'marital-status': ['Divorced', 'Divorced', 'Separated', 'Widowed', 'Separated', 'Divorced', 'Widowed']
+    "age": [0, 120],
+    "income": [">50K", "<=50K"]
 })
+
 # TESTS
 
 def test_plot_exist():
     generate_bar_chart_and_save(test_df, 
-    'age', 
-    'Age',
-    test_dir, 
-    plot_1)
+        'Age',
+        'age', 
+        test_dir, 
+        plot_1)
     file_path = os.path.join('tests/folder_plot', plot_1)
     assert os.path.exists(file_path), f"Plot was not saved at {file_path}"
     assert os.path.getsize(file_path) > 0, f"The saved plot exist but is empty at {file_path}"
